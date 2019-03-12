3240084831e298fad30dfe74fd3a5bc2ad8918c1
Willy <willy008093@gmail.com>
2019-03-13 01:29:41 +0800

    'new'

--- app.py
+++ app.py
@@ -33,7 +33,11 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='aaaa')
+    # message = TextSendMessage(text=event.message.text)
+    if event.message.text == 'HONDA':
+        message = TextSendMessage(text='hrv')
+    else:
+        message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
