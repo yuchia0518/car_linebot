44dbfe7312d35e91afa8d92e383f9a51cd002787
Willy <willy008093@gmail.com>
2019-03-13 00:52:48 +0800

    make it better

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# encoding: utf-8
 from flask import Flask, request, abort
 
 from linebot import (
@@ -7,51 +6,37 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
-handler = WebhookHandler('Your_Channel_Secret') 
-line_bot_api = LineBotApi('Your_Channel_Access_Token') 
+# Channel Access Token
+line_bot_api = LineBotApi('PpsikGBkL6NldjaoIr6ZeFkJvQrcce8zbpFrDUpqOIl7NuX7RIVRob9DeVxZV+1TsUQvlqJ6mUG1nv2njG2O4Yxn9+mSYPofmV3X1ywk8WubSOQQil/A99S77ZxCWV97eExQtYq0wliPyGGD7ndweAdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('f88ac77ef47c8bd99c2607f38c13bebe')
 
-
-@app.route('/')
-def index():
-    return "<p>Hello World!</p>"
-
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-# ================= 機器人區塊 Start =================
-@handler.add(MessageEvent, message=TextMessage)  # default
-def handle_text_message(event):                  # default
-    msg = event.message.text #message from user
-
-    # 針對使用者各種訊息的回覆 Start =========
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=msg))
-
-    # 針對使用者各種訊息的回覆 End =========
-
-# ================= 機器人區塊 End =================
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
