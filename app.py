df350f4620507a8d24de776ed3a919ee0091e17b
Willy <willy008093@gmail.com>
2019-03-13 01:22:31 +0800

    make it better

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ import os
 if __name__ == "__main__":
     # port = int(os.environ.get('PORT', 5000))
     port=os.environ['PORT']
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
